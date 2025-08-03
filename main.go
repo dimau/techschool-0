6968afeb9092785243f0a6cd9be61f9c59d7e155
Dmitrii Ushakov <dimau777@gmail.com>
2025-08-03 16:37:36 +0300

    feat: add worker pool generic

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"techschool-0/workerpool"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Create a new Worker Pool
+	pool := workerpool.NewPool(5)
+
+	// Create a lot of Tasks
+	go func() {
+		for {
+			taskID := rand.Intn(100) + 20
+
+			if taskID%7 == 0 {
+				pool.Stop()
+			}
+
+			time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+			task := workerpool.NewTask(func(data interface{}) error {
+				taskID := data.(int)
+				time.Sleep(100 * time.Millisecond)
+				fmt.Printf("Task %d processed\n", taskID)
+				return nil
+			}, taskID)
+			pool.AddTask(task)
+		}
+	}()
+
+	// Run our Worker Pool
+	pool.RunBackground()
 }
